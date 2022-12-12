86e1cc490d3075e0c6ae74757aa33adb7e2a45c3
BabaiDI <dimababai2004@gmail.com>
2022-12-12 19:31:50 +0200

    add project

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,123 @@
+﻿using ConsoleApp1;
+
+internal class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Введите сет. Пример: 1, 2, 5, 7, 10\n");
+        if (Console.ReadLine() is not string com)
+            return;
+
+        List<int> ints = com.Split(',').Select(int.Parse).ToList();
+
+        Set set = new(ints);
+
+        while (true)
+        {
+            Console.WriteLine($"1: Посмотреть оригинальний сет\n" +
+                              $"2: Посмотреть свой сет\n" +
+                              $"3: Виделить сет\n" +
+                              $"4: Вивести обьединение\n" +
+                              $"5: Вивести разницу\n" +
+                              $"6: Вивести пересичение\n" +
+                              $"7: Удалить сет\n" +
+                              $"0: Выйти\n");
+            Console.Write("Введите команду: ");
+
+            int[]? range;
+
+            switch (Console.ReadLine())
+            {
+                case ("1"):
+                    set.SeeOriginal();
+                    break;
+                case ("2"):
+                    set.SeeSets();
+                    break;
+                case ("3"):
+                    Console.WriteLine("Введите числа. Пример: 1, 0, 5, 3\n");
+                    if (Console.ReadLine() is string intRange)
+                    {
+                        try
+                        {
+                            set.GetSetFromOriginal(intRange);
+                        }catch(Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    break;
+                case ("4"):
+                    range = getRange();
+                    try
+                    {
+                        List<int> union = set.Union(range[0], range[1]);
+                        set.PrintSet(union);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case ("5"):
+                    range = getRange();
+                    try
+                    {
+                        List<int> union = set.Difference(range[0], range[1]);
+                        set.PrintSet(union);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case ("6"):
+                    range = getRange();
+                    try
+                    {
+                        List<int> union = set.Intersection(range[0], range[1]);
+                        set.PrintSet(union);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    break;
+                case ("7"):
+                    Console.WriteLine("Введите индекс\n");
+                    if (Console.ReadLine() is string index)
+                    {
+                        try
+                        {
+                            set.Remove(int.Parse(index));
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine(ex.Message);
+                        }
+                    }
+                    break;
+                case ("0"):
+                    return;
+                default:
+                    break;
+            }
+        }
+
+        int[]? getRange()
+        {
+            Console.WriteLine("Введите индкси. Пример: 1, 0; 2, 3\n");
+            if (Console.ReadLine() is string com)
+            {
+                int[] ints = com.Split(',').Select(int.Parse).ToArray();
+                if (ints.Length > 2)
+                {
+                    Console.WriteLine("Много чисел");
+                    return default;
+                }
+                return ints;
+            }
+            return default;
+        }
+    }
+}
---
+++ ConsoleApp1/Set.cs
@@ -0,0 +1,87 @@
+﻿namespace ConsoleApp1;
+
+internal class Set
+{
+    private SortedSet<int> _original = new();
+
+    private List<SortedSet<int>> _sets = new();
+
+    public Set(List<int> set)
+    {
+        foreach (int item in set)
+        {
+            _original.Add(item);
+        }
+    }
+
+    public List<int> Union(int f, int s)
+    {
+        SortedSet<int> union = new(_sets[f]);
+        foreach (int set in _sets[s])
+        {
+            union.Add(set);
+        }
+        return union.ToList();
+    }
+
+    public List<int> Difference(int f, int s)
+    {
+        SortedSet<int> diff = new(_sets[f]);
+        foreach (int set in _sets[s])
+        {
+            diff.Remove(set);
+        }
+        return diff.ToList();
+    }
+
+    public List<int> Intersection(int f, int s)
+    {
+        SortedSet<int> inter = new(_sets[f]);
+        return inter.Intersect(_sets[s]).ToList();
+    }
+
+    public void SeeOriginal()
+    {
+        PrintSet(_original.ToList());
+    }
+
+    public void PrintSet(List<int> set)
+    {
+        Console.Write("\n");
+        int s = 0;
+        foreach (var item in set)
+        {
+            Console.Write($"{item}\t");
+            s++;
+            if (s % 5 == 0)
+                Console.Write("\n");
+        }
+        Console.Write("\n");
+    }
+
+    public void SeeSets()
+    {
+        for (int i = 0; i < _sets.Count; i++)
+        {
+            Console.WriteLine($"set #{i}");
+            PrintSet(_sets[i].ToList());
+        }
+        Console.Write("\n");
+    }
+
+    public void GetSetFromOriginal(string set)
+    {
+        SortedSet<int> numbers = new(set.Split(',').Select(int.Parse));
+        if (!numbers.All(n => _original.Contains(n)))
+        {
+            throw new Exception("Нельзя виделить сет");
+        }
+
+        _sets.Add(numbers);
+    }
+
+    public void Remove(int index) 
+    {
+        _sets.RemoveAt(index);
+    }
+}
